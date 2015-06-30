037bdb1e0f90aa4ddefe277869477ec85d2b4449
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-06-30 06:16:38 +0300

    Need more sleep...<_<

--- app/common/js/filters/slice.js
+++ app/common/js/filters/slice.js
@@ -6,34 +6,6 @@ var filtersModule = angular.module("app.common.filters");
 
 filtersModule.filter
 (
-    'dateFilter',
-    [
-        function() 
-        {   
-            return function(input) 
-            {
-                return moment(input).locale("ru").format("DD MMMM");
-            };
-        }
-    ] 
-);
-
-filtersModule.filter
-(
-    'timeFilter',
-    [
-        function() 
-        {   
-            return function(input) 
-            {
-                return moment(input).locale("ru").format("dddd, HH:mm");
-            };
-        }
-    ] 
-);
-
-filtersModule.filter
-(
 	'sliceFilter', 
 	[
 		function() 
